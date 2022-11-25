6a5fb155f4968762cd70fd3d75e90f1544144664
irvan taufik <irvantaufik28@gmail.com>
2022-11-25 19:29:47 +0700

    push

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME ?? '127.0.0.1',
-    port: process.env.DB_PORT ?? 5432,
-    dialect: process.env.DB_DIALECT,
-    use_env_variable : 'postgres://vjnpmutpbwazij:7f04ab8896e9656ab9a6b400d51d89a4bd1d172e1dcb04c67a29c379a89c0479@ec2-34-227-120-79.compute-1.amazonaws.com:5432/ddioo3f9vk2lgg'
-  },
-  production : {
-    use_env_variable : 'postgres://vjnpmutpbwazij:7f04ab8896e9656ab9a6b400d51d89a4bd1d172e1dcb04c67a29c379a89c0479@ec2-34-227-120-79.compute-1.amazonaws.com:5432/ddioo3f9vk2lgg'
+    use_env_variable : process.env.DATABASE_URL,
   }
 };
--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,17 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+
+
+let sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: false
+    }
+  });
+
 
 fs.readdirSync(__dirname)
   .filter((file) => {
