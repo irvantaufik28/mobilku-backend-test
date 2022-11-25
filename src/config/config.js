c80115601c7e6fa981bd54fb3c2d34602914e3ee
irvan taufik <irvantaufik28@gmail.com>
2022-11-25 21:10:00 +0700

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,15 @@ module.exports = {
     host: process.env.DB_HOSTNAME ?? '127.0.0.1',
     port: process.env.DB_PORT ?? 5432,
     dialect: process.env.DB_DIALECT,
-    use_env_variable:process.env.DATABASE_URL
   },
+  production: {
+    use_env_variable:process.env.DATABASE_URL,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
+  }
 };
