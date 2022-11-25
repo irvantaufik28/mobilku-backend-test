ebbef281e41acce995d4708a46762893b9c8870c
irvan taufik <irvantaufik28@gmail.com>
2022-11-25 20:42:02 +0700

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,21 +1,20 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: process.env.DATABASE_URL,
-    dialect: "postgres",
-
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME ?? '127.0.0.1',
+    port: process.env.DB_PORT ?? 5432,
+    dialect: process.env.DB_DIALECT,
+    
   },
   production: {
+    
     use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres",
-
+    protocol: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,16 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 }
+);
+
 
 fs.readdirSync(__dirname)
   .filter((file) => {
