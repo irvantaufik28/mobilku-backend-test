28b5b9156f3cbd44cbb6ed4cdeb7ff1d96fa0a7e
irvan taufik <irvantaufik28@gmail.com>
2022-11-25 15:20:05 +0700

    upload image

--- src/libs/mediahandler.js
+++ src/libs/mediahandler.js
@@ -22,8 +22,12 @@ const storage =
                fileFormat: "jpg",
                quality: 80,
                resize: { width: 500, height: 500 },
-               resize: { width: 1000, height: 1000 },
-                 }
+                 },
+                 imageOptions:{
+                    fileFormat: "jpg",
+                    quality: 80,
+                    resize: { width: 1000, height: 1000 },
+                      }
            });
 const upload  =  multer({ storage });
 
@@ -33,12 +37,4 @@ const upload  =  multer({ storage });
 module.exports = upload
 
 
-// const fs = require('fs');
-// const multer = require('multer');
-// const sharp = require('sharp');
 
-// const storage = ({
-//     fs.rename(req.file.path, './images/avatar.jpg', (err)=>{
-//         console.log(err);
-//     })
-// })
--- src/routes/urlFoto.js
+++ src/routes/urlFoto.js
@@ -7,3 +7,23 @@ const mediahandler = require('../libs/mediahandler')
 router.post('/urlfoto', mediahandler.single('url'), urlFoto.urlFotoCreate)
 
 module.exports = router;
+
+
+
+// app.post('/upload', upload.single("avatar"), (req, res)=>
+// {
+//     fs.rename(req.file.path, './images/avatar.jpg', (err)=>{
+//         console.log(err);
+//     })
+  
+//     sharp(__dirname + '/images/avatar.jpg').resize(200,200)
+//     .jpeg({quality : 50}).toFile(__dirname 
+//         + '/images/avatar_thumb.jpg');
+  
+  
+//     sharp(__dirname + '/images/avatar.jpg').resize(640,480)
+//     .jpeg({quality : 80}).toFile(__dirname 
+//         + '/images/avatar_preview.jpg');
+  
+//     return res.json("File Uploaded Successfully!");
+// });
