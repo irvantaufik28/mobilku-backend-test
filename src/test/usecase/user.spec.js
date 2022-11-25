bb0a66907b2410358545133eb7ea356b05f4f993
irvan taufik <irvantaufik28@gmail.com>
2022-11-25 16:00:24 +0700

    age fixing

--- src/test/usecase/user.spec.js
+++ src/test/usecase/user.spec.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const UserUseCase = require('../../usecase/user')
 const mockUser = require('../mock/user.mock')
+const mockUrl = require('../mock/urlFoto.mock')
 
-let mockUserReturn = {}
+let mockUserReturn, mockUrlFotoReturn = {}
 let userUC = null
 
 describe("USER TEST", () => {
@@ -14,8 +15,11 @@ describe("USER TEST", () => {
             updateUser: jest.fn().mockReturnValue(true),
             deletUser: jest.fn().mockReturnValue(true),
         }
+        mockUrlFotoReturn  = {
+            getFotoByUserId: jest.fn().mockReturnValue(mockUrl.urlFoto),
+        }
 
-        userUC = new UserUseCase(mockUserReturn)
+        userUC = new UserUseCase(mockUserReturn, mockUrlFotoReturn)
     })
     describe("Get All User Test", () => {
         test("should isSuccess = true statusCode = 200, and type data is array", async () => {
@@ -24,12 +28,12 @@ describe("USER TEST", () => {
             expect(res.isSuccess).toBeTruthy();
             expect(res.statusCode).toEqual(200);
             expect(Array.isArray(res.data)).toBeTruthy();
-            expect(res.data[0]).toHaveProperty("nama");
-            expect(res.data[0]).toHaveProperty("asal_Kota");
-            expect(res.data[0]).toHaveProperty("no_Whatsapp");
-            expect(res.data[0]).toHaveProperty("pendidikan_terakhir");
-            expect(res.data[0]).toHaveProperty("tanggal_lahir");
-            expect(res.data[0]).toHaveProperty("url_foto");
+            expect(res.data[0]).toHaveProperty("name");
+            expect(res.data[0]).toHaveProperty("city");
+            expect(res.data[0]).toHaveProperty("phone");
+            expect(res.data[0]).toHaveProperty("educationLevel");
+            expect(res.data[0]).toHaveProperty("birth");
+        
         })
 
         test("should isSuccess = true statusCode = 200, and type data is array if data user not available", async () => {
@@ -48,12 +52,12 @@ describe("USER TEST", () => {
 
             expect(res.isSuccess).toBeTruthy();
             expect(res.statusCode).toEqual(200);
-            expect(res.data).toHaveProperty("nama");
-            expect(res.data).toHaveProperty("asal_Kota");
-            expect(res.data).toHaveProperty("no_Whatsapp");
-            expect(res.data).toHaveProperty("pendidikan_terakhir");
-            expect(res.data).toHaveProperty("tanggal_lahir");
-            expect(res.data).toHaveProperty("url_foto");
+            expect(res.data).toHaveProperty("name");
+            expect(res.data).toHaveProperty("city");
+            expect(res.data).toHaveProperty("phone");
+            expect(res.data).toHaveProperty("educationLevel");
+            expect(res.data).toHaveProperty("birth");
+            expect(res.data).toHaveProperty("urlFoto");
         })
 
         test("should isSuccess = true statusCode = 200, and type data is array if data user not available", async () => {
@@ -73,27 +77,21 @@ describe("USER TEST", () => {
 
             expect(res.isSuccess).toBeTruthy();
             expect(res.statusCode).toEqual(200);
-            expect(res.data).toHaveProperty("nama");
-            expect(res.data).toHaveProperty("asal_Kota");
-            expect(res.data).toHaveProperty("no_Whatsapp");
-            expect(res.data).toHaveProperty("pendidikan_terakhir");
-            expect(res.data).toHaveProperty("tanggal_lahir");
-            expect(res.data).toHaveProperty("url_foto");
+            expect(res.data).toHaveProperty("name");
+            expect(res.data).toHaveProperty("city");
+            expect(res.data).toHaveProperty("phone");
+            expect(res.data).toHaveProperty("educationLevel");
+            expect(res.data).toHaveProperty("birth");
+        
         })
     })
 
     describe("Update User", () => {
         test("should isSuccess = true statusCode = 200, and type data is Object ", async () => {
-            let res = await userUC.updateUser({ nama: "customerUpdate" })
+            let res = await userUC.updateUser(1 ,{ nama: "customerUpdate" })
 
             expect(res.isSuccess).toBeTruthy();
             expect(res.statusCode).toEqual(200);
-            expect(res.data).toHaveProperty("nama");
-            expect(res.data).toHaveProperty("asal_Kota");
-            expect(res.data).toHaveProperty("no_Whatsapp");
-            expect(res.data).toHaveProperty("pendidikan_terakhir");
-            expect(res.data).toHaveProperty("tanggal_lahir");
-            expect(res.data).toHaveProperty("url_foto");
         })
 
         test("should isSuccess = false statusCode = 404, user not found ", async () => {
@@ -103,7 +101,6 @@ describe("USER TEST", () => {
             expect(res.isSuccess).toBeFalsy();
             expect(res.statusCode).toEqual(404);
             expect(res.reason).toEqual('user not found')
-            expect(res.data).toEqual(null)
         })
     })
 
--- src/usecase/user.js
+++ src/usecase/user.js
@@ -1,113 +1,117 @@
 class userUseCase {
-    constructor (userRepository, urlFotoRepository) {
-        this.userRepository = userRepository;
-        this.urlFotoRepository = urlFotoRepository;
+  constructor(userRepository, urlFotoRepository) {
+    this.userRepository = userRepository;
+    this.urlFotoRepository = urlFotoRepository;
+  }
+
+  async getAllUser(filter) {
+    let result = {
+      isSuccess: true,
+      statusCode: 200,
+      data: [],
+    };
+
+    const userResult = await this.userRepository.getAllUser(filter);
+
+    result.data = userResult;
+    return result;
+  }
+
+  async getUserById(id) {
+    let result = {
+      isSuccess: false,
+      statusCode: 404,
+      reason: null,
+      data: null,
+    };
+
+    const userResult = await this.userRepository.getUserById(id);
+    if (userResult === null) {
+      result.reason = "user not found";
+      return result;
     }
 
-    async getAllUser(filter){
-        let result = {
-            isSuccess: true,
-            statusCode: 200,
-            data: []
-        }
+    const urlFoto = await this.urlFotoRepository.getFotoByUserId(id);
+    const neWuser = {
+      id: userResult.id,
+      name: userResult.name,
+      birth: userResult.birth,
+      age: userResult.age,
+      phone: userResult.phone,
+      city: userResult.city,
+      educationLevel: userResult.educationLevel,
+      urlFotoId: urlFoto.id,
+      urlFoto,
+    };
 
-        const userResult = await this.userRepository.getAllUser(filter);
-        
-        result.data = userResult
-        return result
+    result.isSuccess = true;
+    result.statusCode = 200;
+    result.data = neWuser;
+    return result;
+  }
+
+  async createUser(userData) {
+    let result = {
+      isSuccess: false,
+      statusCode: 404,
+      reason: null,
+      data: null,
+    };
+
+    const getAge = Math.floor(new Date() - new Date(userData.birth).getTime());
+    const newAge = getAge / 31556926000
+    userData.age = (Math.floor(newAge));
+
+    const userResult = await this.userRepository.createUser(userData);
+
+    result.isSuccess = true;
+    result.statusCode = 200;
+    result.data = userResult;
+    return result;
+  }
+
+  async updateUser(userData, id) {
+    let result = {
+      isSuccess: false,
+      statusCode: 404,
+      reason: null,
+    };
+    const getUser = await this.userRepository.getUserById(id);
+    if (getUser === null) {
+      result.reason = "user not found";
+      return result;
     }
 
-    async getUserById(id){
-        let result = {
-            isSuccess: false,
-            statusCode: 404,
-            reason: null,
-            data: null
-        }
+    await this.userRepository.updateUser(userData, id);
 
-        const userResult = await this.userRepository.getUserById(id);
-        if (userResult === null) {
-            result.reason = 'user not found';
-            return result;
-        }
+    result.isSuccess = true;
+    result.statusCode = 200;
+    return result;
+  }
 
-        const urlFoto = await this.urlFotoRepository.getFotoByUserId(id)
-        const neWuser = {
-            id: userResult.id,
-            name: userResult.name,
-            birth: userResult.birth,
-            age: userResult.age,
-            phone: userResult.phone,
-            city: userResult.city,
-            educationLevel: userResult.educationLevel,
-            urlFotoId:urlFoto.id,
-            urlFoto
-        }
-
-        result.isSuccess = true;
-        result.statusCode = 200;
-        result.data = neWuser
-        return result
+  async deleteUser(id) {
+    let result = {
+      isSuccess: false,
+      statusCode: 404,
+      reason: null,
+      data: null,
+    };
+    const getUser = await this.userRepository.getUserById(id);
+    if (getUser === null) {
+      result.reason = "user not found";
     }
 
-    
-    async createUser(userData){
-        let result = {
-            isSuccess: false,
-            statusCode: 404,
-            reason: null,
-            data: null
-        }
+    await this.userRepository.deleteUser(id);
 
-        const userResult = await this.userRepository.createUser(userData);
+    result.isSuccess = true;
+    result.statusCode = 200;
+    result.data = userResult;
+    return result;
+  }
 
-        result.isSuccess = true;
-        result.statusCode = 200;
-        result.data = userResult
-        return result
-    }
-
-    async updateUser(userData, id){
-        let result = {
-            isSuccess: false,
-            statusCode: 404,
-            reason: null,
- 
-        }
-        const getUser = await this.userRepository.getUserById(id)
-        if (getUser === null) {
-            result.reason = "user not found"
-            return result
-        }
-
-        await this.userRepository.updateUser(userData, id)
-
-
-        result.isSuccess = true;
-        result.statusCode = 200;
-        return result
-    }
-
-    async deleteUser(userData, id){
-        let result = {
-            isSuccess: false,
-            statusCode: 404,
-            reason: null,
-            data: null
-        }
-        const getUser = await this.userRepository.getUserById(id)
-        if (getUser === null) {
-            result.reason = "user not found"
-        }
-
-        await this.userRepository.deleteUser(userData, id)
-
-
-        result.isSuccess = true;
-        result.statusCode = 200;
-        result.data = userResult
-        return result
-    }
+  async getAge(birthDate) {
+   await Math.floor(new Date() - new Date(birthDate).getTime());
+  }
 }
 
 module.exports = userUseCase;
