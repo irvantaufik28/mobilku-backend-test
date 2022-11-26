ee8fbe984c679608d0a38b95b32c9d1d375b1b99
irvan taufik <irvantaufik28@gmail.com>
2022-11-26 08:43:33 +0700

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config()
 
 module.exports = {
   development: {
@@ -28,4 +28,4 @@ module.exports = {
       rejectUnauthorized: false,
     },
   },
-};
+}
